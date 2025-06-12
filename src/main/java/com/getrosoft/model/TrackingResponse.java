abf92ca364cd75d079377684bd0f4551e4617fc1
Amol Kute <amolmadhukarkute@gmail.com>
2025-06-12 17:03:37 +0530

    JUnit added for controller

--- src/main/java/com/getrosoft/model/TrackingResponse.java
+++ src/main/java/com/getrosoft/model/TrackingResponse.java
@@ -2,11 +2,13 @@ package com.getrosoft.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.OffsetDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TrackingResponse {
     private String tracking_number;
     private OffsetDateTime created_at;
